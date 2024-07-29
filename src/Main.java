d1197f1cd78991a49da4e1596b0420efbb129385
JaramilloT <jaramillotamayosantiago@gmail.com>
2024-07-28 22:47:47 -0500

    avance

--- src/Main.java
+++ src/Main.java
@@ -11,10 +11,119 @@ public class Main {
         return y;
     }
 
+    public static int tercero () {
+        double d = 100.04;
+        long l = (long)d;
+        int i = (int)l;
+        return i;
+    }
+
     public static int cuarto () {
         String x = "22";
         int y = Integer.parseInt(x);
         return y;
+    }
+
+    public static int quinto () {
+        int a = 2;
+        char b = (char) a;
+        byte c = (byte) b;
+        return c;
+    }
+
+    public static int sexto () {
+        int a = 65;
+        char  b = (char) a;
+        return  b;
+
+    }
+
+    public static int septimo () {
+        long a = 123;
+        int b = (int) a;
+        return b;
+    }
+
+    public static int octavo () {
+        float a = 123.5F;
+        String  b = new String(Float.toString(a));
+        return Integer.parseInt(b);
+    }
+
+    public static int noveno () {
+    int a = (int) Math.round(3.7);
+        return a;
+    }
+
+    public static int decimo () {
+        byte a = (byte) Math.round(3.7);
+        return a;
+    }
+
+    public static int once () {
+        int x = 10;
+        x += 5;
+        return x;
+    }
+
+    public static int doce () {
+        int x = 2 ;
+        int y = x * 5;
+         y *= y;
+        return y;
+    }
+
+    public static int trece () {
+        int a = 15;
+        a %= 4;
+        return a;
+    }
+
+    public static int catorce () {
+        int x = 2;
+        int y = 2;
+        x += y;
+        return x;
+    }
+
+    public static int quince () {
+        int x = 2;
+        x ^= 2;
+        return x;
+    }
+
+    public static int diesciseis () {
+        int i = 5;
+        i += ++i + i++ + ++i;
+        return i;
+    }
+
+    public static int diescisiete () {
+        double a = 5.6;
+        int b = (int) a;
+        b += ++b ;
+        return b;
+    }
+
+    public static int diesciocho () {
+        double d = 5.7;
+        int i = (int)d;
+        i *= 2;
+        System.out.println(i--);
+        return i;
+    }
+
+    public static int diescinueve() {
+        int a = 2;
+        double b = a * 2.0;
+        return (int) b;
+    }
+
+    public static int veinte() {
+        char c = 'X';
+        c += 32;
+        System.out.println(c);
+        return (int) c;
     }
 }
 
--- src/solve.java
+++ src/solve.java
@@ -8,25 +8,74 @@ public class solve {
         int option;
 
 
-        do {System.out.println("Menu");
+        do {System.out.println("Menu, 1).\n 2).\n 3).\n 4).\n 5).\n 6).\n 7).\n 8).\n 9).\n 10).\n 11).\n 12).\n 13).\n 14).\n 15).\n 16).\n 17).\n 18).\n 19).\n 20).\n");
 
         option = scanner.nextInt();
-
-        switch (option){
+        switch (option) {
             case 1:
-                System.out.println("¿Cuál será el resultado de la siguiente operación?: "+ Main.primero());
+                System.out.println("1). ¿Cuál será el resultado de la siguiente operación?: " + Main.primero());
                 break;
             case 2:
-                System.out.println("Dado char c = 'A';, ¿cómo convertirías c a su valor ASCII como un int?: "+ Main.segundo());
+                System.out.println("2). Dado char c = 'A';, ¿cómo convertirías c a su valor ASCII como un int?: " + Main.segundo());
                 break;
             case 3:
-                System.out.println(""+ Main.tercero());
+                System.out.println("3). ¿Qué valor se imprimirá? " + Main.tercero());
                 break;
             case 4:
-                System.out.println("Escribe código para convertir un String que contiene un número a un int: "+ Main.cuarto());
+                System.out.println("1). Escribe código para convertir un String que contiene un número a un int: " + Main.cuarto());
+                break;
+            case 5:
+                System.out.println("5). ¿Cuál es el resultado de (int) (char) (byte) -1?: " + Main.quinto());
+                break;
+            case 6:
+                System.out.println("6). Convierte el número 65 a un char. ¿Qué carácter obtienes?: "+ Main.sexto());
+                break;
+            case 7:
+                System.out.println("7). ¿Qué sucede si intentas asignar un long a un int sin casteo explícito?: "+ Main.septimo());
+                break;
+            case 8:
+                System.out.println("8). Escribe código para convertir un float a un String.: "+ Main.octavo());
+                break;
+            case 9:
+                System.out.println("9). ¿Cuál es el resultado de (int) Math.round(3.7)? " + Main.noveno());
+                break;
+            case 10:
+                System.out.println("10). Dado byte b = 50;, ¿cómo lo convertirías a short? " + Main.decimo());
+                break;
+            case 11:
+                System.out.println("11). ¿Cuál es el valor de x después de la siguiente operación? int x = 10; x += 5: " + Main.once());
+                break;
+            case 12:
+                System.out.println("12). Simplifica la siguiente expresión usando un operador de asignación compuesta: y = y * (x + 5): " + Main.doce());
+                break;
+            case 13:
+                System.out.println("13). ¿Cuál es el valor de a después de estas operaciones? int a = 15; a %= 4;: " + Main.trece());
+                break;
+            case 14:
+                System.out.println("14). Escribe una expresión equivalente a x = x + y usando un operador de asignación compuesta. : " + Main.catorce());
+                break;
+            case 15:
+                System.out.println("15). ¿Qué hace la siguiente operación? x ^= 2;  : " + Main.quince());
+                break;
+            case 16:
+                System.out.println("16). ¿Cuál es el resultado de la siguiente expresión? : " + Main.diesciseis());
+                break;
+            case 17:
+                System.out.println("17). Escribe código para convertir un double a un int, y luego incrementa el resultado en 1 usando el operador de postincremento. : " + Main.diescisiete());
+                break;
+            case 18:
+                System.out.println("18). ¿Qué se imprimirá en la consola? : " + Main.diesciocho());
+                break;
+            case 19:
+                System.out.println("19). Crea una expresión que use casteo y un operador de asignación compuesta para dividir un int por 2.0 y asignar el resultado a una variable double. : " + Main.diescinueve ());
+                break;
+            case 20:
+                System.out.println("20). ¿Qué se imprimirá en la consola? : " + Main.veinte());
                 break;
         }
+
         }while ( (option !=0));
+
 
     }
 
